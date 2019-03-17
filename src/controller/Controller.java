51c958cc5b65d9f62d275f1f6cca6acb29110d14
Boris Alfredo <Boris Alfredo@DESKTOP-FEP26UH>
2019-03-17 15:37:50 -0500

    Correcciones clase VOMovingviolations

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,21 +2,29 @@ package controller;
 
 import java.util.Scanner;
 
+
 import model.data_structures.IQueue;
 import model.data_structures.IStack;
 import model.vo.VODaylyStatistic;
 import model.vo.VOMovingViolations;
 import view.MovingViolationsManagerView;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+
 
 public class Controller {
- 
+
 	private MovingViolationsManagerView view;
-	
+
 	/**
 	 * Cola donde se van a cargar los datos de los archivos
 	 */
 	private IQueue<VOMovingViolations> movingViolationsQueue;
-	
+
 	/**
 	 * Pila donde se van a cargar los datos de los archivos
 	 */
@@ -25,60 +33,60 @@ public class Controller {
 
 	public Controller() {
 		view = new MovingViolationsManagerView();
-		
+
 		//TODO, inicializar la pila y la cola
 		movingViolationsQueue = null;
 		movingViolationsStack = null;
 	}
-	
+
 	public void run() {
 		Scanner sc = new Scanner(System.in);
 		boolean fin = false;
-		
+
 		while(!fin)
 		{
 			view.printMenu();
-			
+
 			int option = sc.nextInt();
-			
+
 			switch(option)
 			{
-				case 1:
-					this.loadMovingViolations();
-					break;
-					
-//				case 2:
-//					IQueue<VODaylyStatistic> dailyStatistics = this.getDailyStatistics();
-//					view.printDailyStatistics(dailyStatistics);
-//					break;
-					
-				case 2:
-					view.printMensage("Ingrese el número de infracciones a buscar");
-					int n = sc.nextInt();
+			case 1:
+				this.loadMovingViolations();
+				break;
 
-					IStack<VOMovingViolations> violations = this.nLastAccidents(n);
-					view.printMovingViolations(violations);
-					break;
-											
-				case 4:	
-					fin=true;
-					sc.close();
-					break;
+				//				case 2:
+				//					IQueue<VODaylyStatistic> dailyStatistics = this.getDailyStatistics();
+				//					view.printDailyStatistics(dailyStatistics);
+				//					break;
+
+			case 2:
+				view.printMensage("Ingrese el número de infracciones a buscar");
+				int n = sc.nextInt();
+
+				IStack<VOMovingViolations> violations = this.nLastAccidents(n);
+				view.printMovingViolations(violations);
+				break;
+
+			case 4:	
+				fin=true;
+				sc.close();
+				break;
 			}
 		}
 	}
 
-	
+
 
 	public void loadMovingViolations() {
 		
 	}
-	
+
 	public IQueue <VODaylyStatistic> getDailyStatistics () {
 		// TODO
 		return null;
 	}
-	
+
 	public IStack <VOMovingViolations> nLastAccidents(int n) {
 		// TODO
 		return null;
